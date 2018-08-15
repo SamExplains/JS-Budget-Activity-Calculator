89e9ce0173b39841db1e3a431b9fa422504c52b8
Sampuccino <Sampuccino@gmail.com>
2018-08-14 22:08:12 -0700

    Some more work on event clicking, breaking down DOM objects for easier access and sharing across controllers.

--- app.js
+++ app.js
@@ -14,15 +14,46 @@ var budgetController = (function () {
 
 var UIController = (function () {
   /* UI Controller */
+
+  var DOMStrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputAddButton: '.add__btn'
+  };
+
+  return {
+    getinput: function () {
+      return{
+        $type: $(DOMStrings.inputType).val(),
+        $description: $(DOMStrings.inputDescription).val(),
+        $value: $(DOMStrings.inputValue).val()
+      }
+    },
+
+    getDOMStrings: function () {
+      return DOMStrings;
+    }
+
+  };  
+
+
+
 })();
 
 var controller = (function (budgetCtrl, UICtrl) {
 
-  $('.add__btn').click(function ctrlAddItem () {
-    console.log('Button pressed.')
+  var DOM = UICtrl.getDOMStrings();
 
+  var ctrlAddItem = function () {
+    var input = UIController.getinput();
+    console.log(input);
+  };
+
+  $(DOM.inputAddButton).click(function () {
+    console.log('BUTTON manually pressed.');
+    ctrlAddItem();
   });
-
   $(document).keypress(function (e) {
 
     if(e.keyCode === 13 || e.which === 13 ){
